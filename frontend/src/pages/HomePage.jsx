4aeebe3fc735f2cf9582974db0a90f37747b25a3
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-09-25 18:58:32 +0200

    added why choose us section

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar/Navbar.jsx";
 import { Link } from "react-router-dom";
 import ApartmentCard from "../components/Apartments/ApartmentCard/ApartmentCard.jsx";
+import { PiPanorama as Panorama } from "react-icons/pi";
+import { IoDiamondOutline as Diamond } from "react-icons/io5";
+import { GiReceiveMoney as Investment } from "react-icons/gi";
 
 const Home = () => {
 
@@ -41,6 +44,45 @@ const Home = () => {
                 </div>
             </section>
 
+            {/* Perchè scegliere noi */}
+            <section id="choose-us">
+                <div className="container">
+                    <h2>Why Choose Us</h2>
+                    <h4>Why should you choose our apartment?</h4>
+
+                    <div className="row g-5">
+                        <div className="col-4">
+                            <div className="card">
+                                <div className="icon">
+                                    <Panorama className=" fs-1" />
+                                </div>
+                                <h5>Vision Panorama</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium similique dolorem neque accusantium animi provident, aperiam quibusdam temporibus.</p>
+                            </div>
+                        </div>
+                        <div className="col-4">
+                            <div className="card">
+                                <div className="icon">
+                                    <Diamond className="fs-1" />
+                                </div>
+                                <h5>5 Star Standard Living</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium similique dolorem neque accusantium animi provident, aperiam quibusdam temporibus.</p>
+                            </div>
+                        </div>
+                        <div className="col-4">
+                            <div className="card">
+                                <div className="icon">
+                                    <Investment className="fs-1" />
+                                </div>
+                                <h5>Resonable Investment Rate</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium similique dolorem neque accusantium animi provident, aperiam quibusdam temporibus.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            {/* Sezione appartamenti */}
             <section id="apartments">
                 <div className="container">
                     <h2>Our Homestay</h2>
